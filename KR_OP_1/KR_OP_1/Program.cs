b572f7d958f99385a71ab09ccc1cc1ac2a08e4ad
Nikita Pedorenko <nikitnya1@gmail.com>
2019-04-03 11:10:35 +0300

    Done

---
+++ KR_OP_1/KR_OP_1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KR_OP_1
+{
+    class Program
+    {
+        static string inputPath = "Input.txt";
+
+        static List<int> ParseNumbers(string inputPath)
+        {
+            int lines = NumberOfLines(inputPath);
+            StreamReader sr = new StreamReader(inputPath);
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < lines; i++)
+            {
+                string line = sr.ReadLine();
+                foreach (var number in line.Split( new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    numbers.Add(int.Parse(number));
+                }
+            }
+            return numbers;
+        }
+
+        static int NumberOfLines(string Path)
+        {
+            int count = 0;
+            using (StreamReader sr = new StreamReader(Path))
+            {
+                while (sr.ReadLine() != null)
+                    count++;
+            }
+            return count;
+        }
+
+        static void SaveToFiles(List<int> numbers)
+        {
+            foreach (var number in numbers)
+            {
+                string path = number.ToString() + ".txt";
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    CheckExistence(path);
+                    sw.WriteLine(NumberConverter.Convert(number));
+                }
+            }
+        }
+
+        static void CheckExistence(string path)
+        {
+            if (!File.Exists(path))
+                File.Create(path);
+        }
+
+        static void Main(string[] args)
+        {
+            var numbers = ParseNumbers(inputPath);
+            SaveToFiles(numbers);
+        }
+    }
+}
