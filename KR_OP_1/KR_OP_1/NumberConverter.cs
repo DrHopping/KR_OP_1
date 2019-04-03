491d172f1637515d7018e9ce2c670917da9b5e93
Nikita Pedorenko <nikitnya1@gmail.com>
2019-04-03 11:20:24 +0300

    Change NumberConverter

--- KR_OP_1/KR_OP_1/NumberConverter.cs
+++ KR_OP_1/KR_OP_1/NumberConverter.cs
@@ -34,19 +34,16 @@ namespace KR_OP_1
 
             if (number > 0)
             {
-                if (words != "")
-                    words += "and ";
-
-                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+                string[] units = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                string[] tens = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
 
                 if (number < 20)
-                    words += unitsMap[number];
+                    words += units[number];
                 else
                 {
-                    words += tensMap[number / 10];
+                    words += tens[number / 10];
                     if ((number % 10) > 0)
-                        words += "-" + unitsMap[number % 10];
+                        words += " " + units[number % 10];
                 }
             }
 
